829b150ea7231fb28dca0bbda76f4975b7bf1578
Mike McGurrin <ViennaMike@users.noreply.github.com>
2019-11-25 10:54:57 -0500

    Add generic secrets module

---
+++ secrets.py
@@ -0,0 +1,14 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+# which would be not great. So, instead, keep it all in this one file and
+# keep it a secret.
+
+secrets = {
+    'ssid' : 'WIFI SSID NAME HERE',             # Keep the two '' quotes around the name
+    'password' : 'WIFI PASSWORD HERE',         # Keep the two '' quotes around password
+    'timezone' : "YOUR TIMEZONE HERE",  # http://worldtimeapi.org/timezones
+    'aio_username' : 'YOUR USERNAME FOR ADAFRUIT AIO', #See learning guide at https://learn.adafruit.com/welcome-to-adafruit-io
+    'aio_key' : 'YOUR KEY FOR AIO',
+    'openweather_token' : 'YOUR OPENWEATHER TOKEN',   # see https://openweathermap.org/api
+    'alpha_advantage_key' : 'YOUR ALPHA ADVANTAGE KEY'   #https://www.alphavantage.co/
+    }
